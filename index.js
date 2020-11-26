adf858e62b386ec97876bf9672a575529aa3cb54
Nullish <richerde43@gmail.com>
2020-11-26 17:36:20 +0000

    Annotations

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*
+
+Receive CSV of IDs and HYML snippets.
+Return a JSON object of the href values extracted to mutiple elements, labelled with the original unique ID.
+
+*/
+
 const csvFilePath='./io/input.csv'
 const csv=require('csvtojson')
 csv()
@@ -5,16 +12,17 @@ csv()
 .then((jsonObj)=>{
 	let jsOut = []
 	for (let i of jsonObj) {
-		let hrefs = i.html.match(/(?<=href=").*?(?=")/g);
+		let hrefs = i.html.match(/(?<=href=").*?(?=")/g); // match hrefs into an array
 		if (hrefs) {
 			for (href of hrefs) {
+				// add matched hrefs to a JS object, as a key value pair, reataining original unique ID
 				let obj = {
 					'linkId': i.linkID,
 					'html': href
 				}
-				jsOut.push(obj);
+				jsOut.push(obj); // Build array of all matches
 			}
-			
+
 		}
 	}
 	console.log(jsOut);
